58041eb835e1c43993186bc7988ba6d924d29900
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-07-31 13:01:43 +0300

    сделала компонент транзакции

---
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import s from 'components/TransactionHistory/TransactionHistory.module.css';
+
+export default function TransactionHistory({ items }) {
+  return (
+    <table className={s.transactionHistory}>
+      <thead className={s.title}>
+        <tr>
+          <th className={s.text}>Type</th>
+          <th className={s.text}>Amount</th>
+          <th className={s.text}>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr className={s.row} key={id}>
+            <td className={s.data}>{type}</td>
+            <td className={s.data}>{amount}</td>
+            <td className={s.data}>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+TransactionHistory.protoTypes = {
+  id: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+  currency: PropTypes.string.isRequired,
+};
